89546b4a5af55e1093cca862fe38077c4582ffc0
asctech90 <>
2026-02-24 23:32:12 +0000

    Challenge: Boolean Logic

--- conditional_statements/challenge_boolean_logic/main.py
+++ conditional_statements/challenge_boolean_logic/main.py
@@ -4,3 +4,14 @@ selling_well = False
 current_stock = 150
 high_stock_threshold = 100
 
+# Step 1: Overstock risk if seasonal AND stock exceeds threshold
+overstock_risk = seasonal and current_stock > high_stock_threshold
+
+# Step 2: Discount eligibility if not selling well AND not already on sale
+discount_eligible = not selling_well and not on_sale
+
+#Step 3: Decide to make a discount if either condition holds
+make_discount = overstock_risk or disscount_eligible
+
+# Step 4: Output the result
+print ("Should the item be discounted?", make_discount)
