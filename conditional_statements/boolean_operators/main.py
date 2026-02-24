86a5ee22adaba0ef58023abf7aea02e9f9b56244
asctech90 <>
2026-02-24 14:13:24 +0000

    Boolean Operators

---
+++ conditional_statements/boolean_operators/main.py
@@ -0,0 +1,7 @@
+total_cost = 25.00
+discount_threshold = 20.00
+
+#Create a boolean variable to found discount eligibility by comparing the total cost variable to the discount threshold using the greater than or equal to operator. 
+discountEligible = total_cost >= discount_threshold
+
+print ("Is the purchase eligible for a discount?", discountEligible)
