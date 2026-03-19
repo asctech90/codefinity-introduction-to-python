95f05c6fe19ed2a9049ce11a0cfa14893cbef365
Arvinder Cheema <asctech90@gmail.com>
2026-03-19 19:19:07 -0400

    removed python project files from develop repository and added manual txt file

--- getting_started/arithmetic_operators/main.py
+++
@@ -1,5 +0,0 @@
-# Calculate addition
-print(5.42 + 4.39) 
-
-# Calculate subtraction
-print(12.30 - 5.50)  
--- getting_started/challenge_multiply_divide/main.py
+++
@@ -1,5 +0,0 @@
-# Calculate multiplication
-print(7.32 * 0.2) 
-
-# Calculate division
-print(8.30 / 20)
